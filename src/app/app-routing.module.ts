cb0943fd4f8ee7f9ad665c1d1cfe4129f6a3db55
eniyavan <71455480+eniyavanweb@users.noreply.github.com>
2020-11-19 20:47:18 -0800

    website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"login",component:LoginComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"about",component:AboutComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
